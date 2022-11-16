91b341d7f82c5676f4aa0041e11507ad0506d58c
zPopeyex <stven-457@hotmail.com>
2022-11-16 15:36:01 -0500

    Add introduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,13 +16,26 @@ const Home: NextPage = () => {
             <div className="card-body">
               <h1>Introduction</h1>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
-                necessitatibus ad consequuntur ratione ipsam quo voluptatem eum
-                ipsa temporibus nostrum ut rerum quod, vel doloribus inventore
-                molestias hic possimus dolores harum sint quam fugit. Esse
-                eveniet itaque aspernatur ex rerum cum hic possimus repellat!
-                Suscipit tenetur ea quis tempora, impedit culpa at delectus
-                veniam dolor ducimus veritatis hic dignissimos necessitatibus?
+                The purpose of this virtual Learning Object focuses on the
+                essential influence of reading both in the mother tongue and in
+                English on the cultural development of citizens, since it is a
+                communicative process that facilitates the human being the
+                possibility of interacting with a globalized world (where
+                English is the universal language) and its common reality that
+                is very permeated by Anglo-Saxon elements, likewise it allows
+                him to express what he thinks, feels and wishes, in order to
+                reinvent himself and transform his context. It is for this
+                reason that, in the first instance, the impact that the media
+                and information technologies are having on the training
+                processes of the new generations is outlined, since we are today
+                concurring in the emergence of a new digital culture, of new
+                ways of reading and writing, of different ways of seeing and
+                hearing, of feeling and enjoying, of learning and knowing. In
+                this way, the design of a Virtual Learning Object becomes a
+                contribution to the strengthening of the literal comprehension
+                competence of short texts in English, taking into account the
+                new forms of reciprocity that students have with technology and
+                the Internet.
               </p>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
