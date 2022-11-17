6995e6284eff91d5527f52476f6d044746de4b7f
zPopeyex <stven-457@hotmail.com>
2022-11-17 04:54:26 -0500

    Clean page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <meta name="description" content="" />
       </Head>
       <div className="row">
-        <div className="col-xl-7 col-xs-12">
+        <div className="col-xl-7 col-xs-12 mb-4">
           <div className="card shadow">
             <div className="card-body">
               <h1>Introduction</h1>
@@ -36,24 +36,6 @@ const Home: NextPage = () => {
                 competence of short texts in English, taking into account the
                 new forms of reciprocity that students have with technology and
                 the Internet.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
-                necessitatibus ad consequuntur ratione ipsam quo voluptatem eum
-                ipsa temporibus nostrum ut rerum quod, vel doloribus inventore
-                molestias hic possimus dolores harum sint quam fugit. Esse
-                eveniet itaque aspernatur ex rerum cum hic possimus repellat!
-                Suscipit tenetur ea quis tempora, impedit culpa at delectus
-                veniam dolor ducimus veritatis hic dignissimos necessitatibus?
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
-                necessitatibus ad consequuntur ratione ipsam quo voluptatem eum
-                ipsa temporibus nostrum ut rerum quod, vel doloribus inventore
-                molestias hic possimus dolores harum sint quam fugit. Esse
-                eveniet itaque aspernatur ex rerum cum hic possimus repellat!
-                Suscipit tenetur ea quis tempora, impedit culpa at delectus
-                veniam dolor ducimus veritatis hic dignissimos necessitatibus?
               </p>
             </div>
           </div>
