3364561ed66aef546d254cb73e60ab3b75a71e6b
sirzes02 <sirzes02@gmail.com>
2022-11-16 22:10:44 -0500

    grammar page

---
+++ pages/grammar.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import Layout from "../src/components/Layout";
+
+const Grammar: NextPage = () => (
+  <Layout>
+    <Head>
+      <title>OVA English - Grammar</title>
+      <meta name="description" content="" />
+    </Head>
+    <div className="d-flex flex-column align-items-center justify-content-center">
+      HOLA
+    </div>
+  </Layout>
+);
+
+export default Grammar;
