8af57e894cea708c4188ef67658740e80129ecb1
sirzes02 <sirzes02@gmail.com>
2022-11-18 00:20:42 -0500

    make it better

--- next.config.js
+++ next.config.js
@@ -24,8 +24,13 @@ const nextConfig = {
     });
 
     config.module.rules.push({
-      test: /\.svg$/i,
-      use: ["@svgr/webpack"],
+      test: [/\.svg$/, /\.woff$/],
+      loader: "file-loader",
+      options: {
+        name: "[name].[hash:8].[ext]",
+        publicPath: `/_next/static/images/`, //specify the base path
+        outputPath: "static/images", //and output path
+      },
     });
     return config;
   },
